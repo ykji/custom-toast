f92ccec660e5b816c34e589a444290bd978e5560
ykji <yvarshney44@gmail.com>
2023-08-24 16:30:52 +0530

    feat: implement custom toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,46 @@
 import "./App.css";
 import { useState } from "react";
-import Toast from "./components/Toast";
+import Toast, { ToastProps } from "./components/Toast";
 
 function App() {
   const [showToast, setShowToast] = useState(false);
+
+  const handleCloseToast = () => {
+    setShowToast(false);
+  };
+
+  const deleteToastProps: ToastProps = {
+    title: "Delete Confirmation",
+    details:
+      "You won't be able to undo this action. Do you want to delete this item?",
+    actions: [
+      {
+        id: 1,
+        name: "Confirm",
+        onClick: () => {
+          console.log("confirm");
+        },
+      },
+      {
+        id: 2,
+        name: "Cancel",
+        onClick: handleCloseToast,
+      },
+    ],
+    color: "#2970FF",
+  };
 
   return (
     <div className="App">
       <button
         className="toast-toggle-btn"
-        onClick={() => setShowToast(!showToast)}
+        onClick={() => {
+          setShowToast(!showToast);
+        }}
       >
         {showToast ? "Hide Toast" : "Show Toast"}
       </button>
-      {showToast && <Toast />}
+      {showToast && <Toast {...deleteToastProps} />}
     </div>
   );
 }
--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -1,7 +1,33 @@
 import "./index.css";
+import { IoIosFlash } from "react-icons/io";
+import { Action } from "../../interface/toast";
 
-const Toast = () => {
-  return <div className="toast">Toast</div>;
+export interface ToastProps {
+  color: string;
+  title: string;
+  details: string;
+  actions: Action[];
+}
+
+const Toast = (props: ToastProps) => {
+  const { title, details, actions, color } = props;
+
+  return (
+    <div className="toast" style={{ backgroundColor: `${color}` }}>
+      <IoIosFlash size={22} />
+      <div className="toast-content">
+        <h4>{title}</h4>
+        <p className="toast-details">{details}</p>
+        <div className="toast-actions">
+          {actions.map(({ name, id, onClick }) => (
+            <button className="toast-action" key={id} onClick={onClick}>
+              {name}
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Toast;
