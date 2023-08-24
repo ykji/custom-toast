4263269353391c1504f8d088fe55ba6c52e3ece9
ykji <yvarshney44@gmail.com>
2023-08-24 15:04:24 +0530

    home setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Toast from "./components/Toast";
 
 function App() {
+  const [showToast, setShowToast] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        className="toast-toggle-btn"
+        onClick={() => setShowToast(!showToast)}
+      >
+        {showToast ? "Hide Toast" : "Show Toast"}
+      </button>
+      {showToast && <Toast />}
     </div>
   );
 }
---
+++ src/components/Toast/index.tsx
@@ -0,0 +1,7 @@
+import "./index.css";
+
+const Toast = () => {
+  return <div className="toast">Toast</div>;
+};
+
+export default Toast;
